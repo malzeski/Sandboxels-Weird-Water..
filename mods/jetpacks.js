d483927ba232bee03f9faa0a2ef290ff2e5cab2b
ConMan122026 <connoramahoney@gmail.com>
2026-05-01 17:46:10 -0400

    Add files via upload

---
+++ mods/jetpacks.js
@@ -0,0 +1,66 @@
+elements.Water_Jetpack = {
+    color: "#1325ca",
+    behavior: [
+        "XX|M1|XX",
+        "M2|LB:water|M2",
+        "XX|XX|XX",
+    ],
+    category: "Jetpacks",
+    state: "solid",
+    stateHigh: "steam",
+    tempHigh: 500,
+    tempLow:0,
+    stateLow: "ice",
+}
+elements.Water_Gas_Jetpack = {
+    color: "#0f4d9e",
+    behavior: [
+        "XX|M1|XX",
+        "M2|LB:water_gas|M2",
+        "XX|XX|XX",
+    ],
+    category: "Jetpacks",
+    state: "solid",
+    stateHigh: "steam",
+    tempHigh: 500,
+    tempLow:0,
+    stateLow: "ice",
+}
+elements.Magma_Jetpack = {
+    color: "#ca5613",
+    behavior: [
+        "XX|M1|XX",
+        "M2|LB:magma|M2",
+        "XX|XX|XX",
+    ],
+    category: "Jetpacks",
+    state: "solid",
+    stateHigh: "fire",
+    tempHigh: 5000,
+    tempLow:0,
+    stateLow: "rock",
+}
+elements.water_gas = {
+    color: "#0751b3",
+    behavior: behaviors.GAS,
+    category: "gases",
+    state: "gas",
+    stateHigh: "steam",
+    tempHigh: 50,
+    tempLow:0,
+    stateLow: "ice",
+}
+elements.Jetpack = {
+    color: "#525252",
+    behavior: [
+        "XX|M1|XX",
+        "M2|LB:fire%20|M2",
+        "XX|XX|XX",
+    ],
+    category: "Jetpacks",
+    state: "solid",
+    stateHigh: "fire",
+    tempHigh: 5000,
+    tempLow:0,
+    stateLow: "rock",
+}
