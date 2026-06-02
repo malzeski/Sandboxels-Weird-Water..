c5cbfa00999f205432bd4024eac1beae17f4fccb
malzeski <eliyahu438@gmail.com>
2026-06-02 03:45:19 -0400

    Update weird_water.js

--- mods/weird_water.js
+++ mods/weird_water.js
@@ -12,7 +12,8 @@ elements.weird_water = {
         "salt": { elem1: "milk", elem2: null, temp1:-20 },
         "sugar": { elem1: "steel", elem2: null },
         "honey": { elem1: "steel" },
-        "caramel": { elem1: "steam", elem2: null } // theres like a billion more reactions here i cut out so i can actually send this
+        "caramel": { elem1: "steam", elem2: null }, // theres like a billion more reactions here i cut out so i can actually send this
+        "bread": { elem1: "mud" },
 },
     state: "liquid",
     density: 997,
